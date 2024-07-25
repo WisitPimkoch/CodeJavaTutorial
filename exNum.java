9d8da17f2944922f732bf5c54af7a9d758eae203
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-07-25 14:31:15 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x,y;
+        x = 62;
+        y = 99;
+
+        int total = x + y;
+
+        System.out.println("Total = " + total);
+    }
+}
