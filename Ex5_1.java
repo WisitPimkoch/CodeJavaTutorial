b27313f105cc53662ee67fbd940a7c50a2cdeacb
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-02 14:59:49 +0700

    Create Ex5_1.java

---
+++ Ex5_1.java
@@ -0,0 +1,16 @@
+public class Ex5_1 {
+
+    public static double calculateTriangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+    public static void main(String[] args) {
+
+        double base = 5.0;
+        double height = 10.0;
+
+        double area = calculateTriangleArea(base, height);
+
+        System.out.println("The area of the triangle is: " + area);
+    }
+}
