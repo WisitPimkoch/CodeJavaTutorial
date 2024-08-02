2d011e7a52591bdf7443b3cb83289d11121cd34a
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-02 10:13:46 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,10 @@
+public class CallName {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            name();
+        }
+    }
+    public static void name() {
+        System.out.println("Wisit Pimkoch");
+    }
+}
