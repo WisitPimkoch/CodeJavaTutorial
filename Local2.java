0b2a09da59f860e25af9e9cbb148077a440d3421
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-02 14:59:58 +0700

    Create Local2.java

---
+++ Local2.java
@@ -0,0 +1,9 @@
+public class Local2 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("x = "+x);
+        one();
+        System.out.println("x = "+x);
+    }
+    
+}
