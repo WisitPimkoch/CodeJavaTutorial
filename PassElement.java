3269aa5e90341d42761064d474c0c2fa3d8ea073
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-07 13:27:03 +0700

    Create PassElement.java

---
+++ PassElement.java
@@ -0,0 +1,11 @@
+public class PassElement {
+    public static void main(String[] args) {
+        int [] numbers = {5,10,15,20,25,30,35,40};
+        for(int i = 0; i < numbers.length; i++){
+            showValue();
+        }
+    }
+    public static void showValue(int n) {
+        System.out.println(n+" ");
+    }
+}
