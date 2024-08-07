1bdec1ca3a6d5899c8b30bb30ba67d91377b16fc
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-07 13:27:09 +0700

    Create TwoArray.java

---
+++ TwoArray.java
@@ -0,0 +1,17 @@
+public class TwoArray {
+    public static void main(String[] args) {
+        int [] [] table = new int [5] [10];
+        for(int row = 0; row < table[row].length; row++){
+            for(int col = 0; col < table[row].length; col++){
+                table[row][col] = row * 10 + col;
+            }
+        }
+        for(int row = 0; row < table[row].length; row++){
+            for(int col = 0; col < table[row].length; col++){
+                System.out.println(table[row][col]+"\t");
+            }
+            System.out.println();
+            
+        }
+    }
+}
