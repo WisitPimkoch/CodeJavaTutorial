d0a21d00029aaa93d685598bdd3f521c799618ba
WisitPimkoch <152240567+WisitPimkoch@users.noreply.github.com>
2024-08-09 13:19:13 +0700

    Create ConstructorDemo.java

---
+++ ConstructorDemo.java
@@ -0,0 +1,11 @@
+public class ConstructorDemo {
+    public static void main(String[] args) {
+      ContRectangle box = new ContRectangle(8.0, 12.0);  
+      System.out.println("Te box's length is "+box.getLength());
+      System.out.println("The box's width is "+box.getWidth());
+      System.out.println("The box's area is "+box.getArea());
+      
+    }
+    
+    
+}
